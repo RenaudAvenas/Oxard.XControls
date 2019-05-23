d076c87d50210564537e7c7015a74efd301f6791
RenaudAvenas <renaud.avenas@outlook.fr>
2019-05-23 18:17:40 +0200

    [1.1.5.5] update nuspec and batch for package generation to update MSBuild path

--- generateNugetPackage.bat
+++ generateNugetPackage.bat
@@ -1,11 +1,11 @@
 echo Build solution
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Oxard.XControls\Oxard.XControls.csproj /t:build /p:Configuration=Release
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Oxard.XControls.Android\Oxard.XControls.Android.csproj /t:build /p:Configuration=Release
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Oxard.XControls.UWP\Oxard.XControls.UWP.csproj /t:build /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" Oxard.XControls\Oxard.XControls.csproj /t:build /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" Oxard.XControls.Android\Oxard.XControls.Android.csproj /t:build /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" Oxard.XControls.UWP\Oxard.XControls.UWP.csproj /t:build /p:Configuration=Release
 echo Build x64 UWP
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Oxard.XControls.UWP\Oxard.XControls.UWP.csproj /t:build /p:Configuration=Release /p:Platform="x64"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" Oxard.XControls.UWP\Oxard.XControls.UWP.csproj /t:build /p:Configuration=Release /p:Platform="x64"
 echo Build ARM UWP
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Oxard.XControls.UWP\Oxard.XControls.UWP.csproj /t:build /p:Configuration=Release /p:Platform="ARM"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" Oxard.XControls.UWP\Oxard.XControls.UWP.csproj /t:build /p:Configuration=Release /p:Platform="ARM"
 
 echo Generate nuget package
 nuget pack Oxard.XControls.nuspec
