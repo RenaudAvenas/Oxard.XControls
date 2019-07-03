252e45fcf7d62afc2adaa33206dc9c42dcf21a23
RenaudAvenas <renaud.avenas@outlook.fr>
2019-07-03 18:55:49 +0200

    [TestApp] Test views automatically added in a master detail page

--- Oxard.TestApp/Oxard.TestApp/MainPage.xaml.cs
+++ Oxard.TestApp/Oxard.TestApp/MainPage.xaml.cs
@@ -1,18 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Xamarin.Forms;
 
 namespace Oxard.TestApp
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage
     {
         public MainPage()
         {
+            this.BindingContext = this;
+
+            var assembly = Assembly.GetExecutingAssembly();
+
+            foreach (var type in assembly.GetTypes())
+            {
+                if (type.FullName.Contains(".Views.") && type.FullName.EndsWith("View"))
+                    this.PageModels.Add(new PageModel(type));
+            }
+
             InitializeComponent();
         }
+
+        public List<PageModel> PageModels { get; } = new List<PageModel>();
 
         private void ButtonOnClicked(object sender, EventArgs e)
         {
             Application.Current.MainPage = new NavigationPage(new MainPage());
+        }
+
+        private void ListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var pageModel = (PageModel)e.SelectedItem;
+            this.CurrentView.Content = pageModel.CreateInstance();
+        }
+
+        public class PageModel
+        {
+            public PageModel(Type pageType)
+            {
+                this.PageType = pageType;
+                this.Name = pageType.Name;
+            }
+
+            public Type PageType { get; }
+
+            public string Name { get; }
+
+            internal View CreateInstance()
+            {
+                return (View)Activator.CreateInstance(this.PageType);
+            }
         }
     }
 }
